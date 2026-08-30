3db6211e9efd48ea5cb95355d96b8be2e0e6b8d7
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:48:49 +0500

    Export mathematical field semantics

--- spectra/domains/mathematics/__init__.py
+++ spectra/domains/mathematics/__init__.py
@@ -1,4 +1,13 @@
 from spectra.domains.mathematics.domain import MathematicsDomain
+from spectra.domains.mathematics.fields import AxisSample, RegularGrid3D, ScalarField3D, VectorField3D
 from spectra.domains.mathematics.functions import Function1D, Interval
 
-__all__ = ["MathematicsDomain", "Function1D", "Interval"]
+__all__ = [
+    "MathematicsDomain",
+    "Function1D",
+    "Interval",
+    "AxisSample",
+    "RegularGrid3D",
+    "ScalarField3D",
+    "VectorField3D",
+]
