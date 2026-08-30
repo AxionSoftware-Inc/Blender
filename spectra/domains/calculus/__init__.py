9c2117d87fb1af04ee1947efb8a2474deb714b12
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 18:24:12 +0500

    Export advanced 2D and Jacobian calculus capabilities

--- spectra/domains/calculus/__init__.py
+++ spectra/domains/calculus/__init__.py
@@ -8,14 +8,27 @@ from spectra.domains.calculus.domain import (
     integrate,
     tangent_at,
 )
+from spectra.domains.calculus.jacobian3d import Jacobian3DDomain, jacobian_at_3d
+from spectra.domains.calculus.vector2d import (
+    VectorCalculus2DDomain,
+    divergence_at_2d,
+    gradient_at_2d,
+    scalar_curl_at_2d,
+)
 
 __all__ = [
     "CalculusDomain",
+    "Jacobian3DDomain",
     "TangentSample",
+    "VectorCalculus2DDomain",
     "curl_at",
     "derivative_at",
     "divergence_at",
+    "divergence_at_2d",
     "gradient_at",
+    "gradient_at_2d",
     "integrate",
+    "jacobian_at_3d",
+    "scalar_curl_at_2d",
     "tangent_at",
 ]
