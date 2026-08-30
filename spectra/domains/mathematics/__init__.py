cd5d30ffdd73f3caca1e1c0facb35f9cd3772977
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 11:01:04 +0500

    Export parametric mathematics semantics

--- spectra/domains/mathematics/__init__.py
+++ spectra/domains/mathematics/__init__.py
@@ -1,12 +1,15 @@
 from spectra.domains.mathematics.domain import MathematicsDomain
 from spectra.domains.mathematics.fields import RegularGrid3D, ScalarField3D, VectorField3D
 from spectra.domains.mathematics.functions import Function1D, Function2D, Interval, RectDomain2D
+from spectra.domains.mathematics.parametric import ParametricCurve3D, ParametricSurface3D
 
 __all__ = [
     "Function1D",
     "Function2D",
     "Interval",
     "MathematicsDomain",
+    "ParametricCurve3D",
+    "ParametricSurface3D",
     "RectDomain2D",
     "RegularGrid3D",
     "ScalarField3D",
