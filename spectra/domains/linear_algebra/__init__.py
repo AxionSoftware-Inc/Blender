43ae5d6c4c3e4641d30f0727463d1bba1cbd5f76
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:44:24 +0500

    Expose complex linear algebra capabilities

--- spectra/domains/linear_algebra/__init__.py
+++ spectra/domains/linear_algebra/__init__.py
@@ -1,9 +1,23 @@
 from spectra.domains.linear_algebra.domain import (
+    ComplexVectorN,
     LinearAlgebraDomain,
     VectorN,
+    complex_inner_product,
+    complex_norm,
     inner_product,
     norm,
     normalize,
+    normalize_complex,
 )
 
-__all__ = ["LinearAlgebraDomain", "VectorN", "inner_product", "norm", "normalize"]
+__all__ = [
+    "ComplexVectorN",
+    "LinearAlgebraDomain",
+    "VectorN",
+    "complex_inner_product",
+    "complex_norm",
+    "inner_product",
+    "norm",
+    "normalize",
+    "normalize_complex",
+]
