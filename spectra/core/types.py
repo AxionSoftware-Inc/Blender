13dce98db255531929ee4ab0d36783bed5af87dc
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 13:53:39 +0500

    Add vector cross product for scientific coordinate geometry

--- spectra/core/types.py
+++ spectra/core/types.py
@@ -41,6 +41,13 @@ class Vec3:
     def dot(self, other: "Vec3") -> float:
         return self.x * other.x + self.y * other.y + self.z * other.z
 
+    def cross(self, other: "Vec3") -> "Vec3":
+        return Vec3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+
     @property
     def magnitude(self) -> float:
         return math.sqrt(self.dot(self))
