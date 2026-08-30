1e446387192822df2a477eb693c8cae4ba486524
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 23:07:13 +0500

    Export reaction-diffusion chemistry API

--- spectra/domains/chemistry/__init__.py
+++ spectra/domains/chemistry/__init__.py
@@ -4,10 +4,24 @@ from spectra.domains.chemistry.domain import (
     ReactionNetwork,
     mass_action_reaction,
 )
+from spectra.domains.chemistry.reaction_diffusion3d import (
+    ReactionDiffusion3DDomain,
+    ReactionDiffusionProblem3D,
+    ReactionDiffusionSolution3D,
+)
+from spectra.domains.chemistry.reaction_diffusion_views3d import (
+    ReactionDiffusionFields3D,
+    ReactionDiffusionViews3DDomain,
+)
 
 __all__ = [
     "ChemicalReaction",
     "ChemistryDomain",
+    "ReactionDiffusion3DDomain",
+    "ReactionDiffusionFields3D",
+    "ReactionDiffusionProblem3D",
+    "ReactionDiffusionSolution3D",
+    "ReactionDiffusionViews3DDomain",
     "ReactionNetwork",
     "mass_action_reaction",
 ]
