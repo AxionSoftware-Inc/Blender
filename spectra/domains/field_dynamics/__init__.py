a826de67707cdad1f0556b0573dd5f4f9b23f3a0
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 18:24:25 +0500

    Export 2D and 3D field dynamics APIs

--- spectra/domains/field_dynamics/__init__.py
+++ spectra/domains/field_dynamics/__init__.py
@@ -4,12 +4,24 @@ from spectra.domains.field_dynamics.domain import (
     IntegralCurveProblem3D,
     PathlineProblem3D,
 )
+from spectra.domains.field_dynamics.domain2d import (
+    CurveSolution2D,
+    FieldDynamics2DDomain,
+    IntegralCurveProblem2D,
+    PathlineProblem2D,
+)
 from spectra.domains.field_dynamics.visualization import compile_curve_solution_scene
+from spectra.domains.field_dynamics.visualization2d import compile_curve_solution_2d_scene
 
 __all__ = [
+    "CurveSolution2D",
     "CurveSolution3D",
+    "FieldDynamics2DDomain",
     "FieldDynamicsDomain",
+    "IntegralCurveProblem2D",
     "IntegralCurveProblem3D",
+    "PathlineProblem2D",
     "PathlineProblem3D",
+    "compile_curve_solution_2d_scene",
     "compile_curve_solution_scene",
 ]
