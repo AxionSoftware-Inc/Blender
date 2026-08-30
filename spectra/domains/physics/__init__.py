4e66743af22253ad3dec06e813e2eb590a6315df
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:49:57 +0500

    Export electromagnetism physics domain

--- spectra/domains/physics/__init__.py
+++ spectra/domains/physics/__init__.py
@@ -1,3 +1,14 @@
+from spectra.domains.physics.electromagnetism import (
+    ElectromagnetismDomain,
+    PointCharge,
+    electric_field_from_point_charges,
+)
 from spectra.domains.physics.quantum import QuantumDomain, QuantumState
 
-__all__ = ["QuantumDomain", "QuantumState"]
+__all__ = [
+    "ElectromagnetismDomain",
+    "PointCharge",
+    "electric_field_from_point_charges",
+    "QuantumDomain",
+    "QuantumState",
+]
