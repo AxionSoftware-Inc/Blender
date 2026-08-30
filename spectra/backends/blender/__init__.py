24e0ecd6149524f55cc024f522622dce327b82b3
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 16:02:05 +0500

    Export Blender timeline playback controller

--- spectra/backends/blender/__init__.py
+++ spectra/backends/blender/__init__.py
@@ -7,11 +7,17 @@ from spectra.backends.blender.incremental import (
     IncrementalBlenderBackend,
     IncrementalBlenderHandle,
 )
+from spectra.backends.blender.timeline import (
+    BlenderTimelineController,
+    frame_to_engine_time,
+)
 
 __all__ = [
     "BlenderBackend",
     "BlenderHandle",
+    "BlenderTimelineController",
     "BlenderUnavailableError",
     "IncrementalBlenderBackend",
     "IncrementalBlenderHandle",
+    "frame_to_engine_time",
 ]
