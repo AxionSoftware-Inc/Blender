e4cec1b2196dec773ec63d11440473ab3a40c27d
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 14:55:21 +0500

    Expose Blender backend without eager bpy import

---
+++ spectra/backends/blender/__init__.py
@@ -0,0 +1,11 @@
+from spectra.backends.blender.backend import (
+    BlenderBackend,
+    BlenderHandle,
+    BlenderUnavailableError,
+)
+
+__all__ = [
+    "BlenderBackend",
+    "BlenderHandle",
+    "BlenderUnavailableError",
+]
