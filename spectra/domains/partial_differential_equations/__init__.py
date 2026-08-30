93d5b7f2fd6fcc2826a40e88a7a7ce93e6e2d2d1
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 17:32:35 +0500

    Export two-dimensional PDE APIs

--- spectra/domains/partial_differential_equations/__init__.py
+++ spectra/domains/partial_differential_equations/__init__.py
@@ -13,21 +13,39 @@ from spectra.domains.partial_differential_equations.domain import (
     UniformGrid1D,
     second_derivative_1d,
 )
+from spectra.domains.partial_differential_equations.domain2d import (
+    BoundaryMode2D,
+    PartialDifferentialEquations2DDomain,
+    ScalarPDEProblem2D,
+    ScalarPDESolution2D,
+    UniformGrid2D,
+    laplacian_2d,
+)
 from spectra.domains.partial_differential_equations.visualization import (
     compile_scalar_pde_solution_scene,
+)
+from spectra.domains.partial_differential_equations.visualization2d import (
+    compile_scalar_pde_solution_2d_scene,
 )
 
 __all__ = [
     "BoundaryMode1D",
+    "BoundaryMode2D",
     "ComplexPDEProblem1D",
     "ComplexPDESolution1D",
     "ComplexPartialDifferentialEquationsDomain",
+    "PartialDifferentialEquations2DDomain",
     "PartialDifferentialEquationsDomain",
     "ScalarPDEProblem1D",
+    "ScalarPDEProblem2D",
     "ScalarPDESolution1D",
+    "ScalarPDESolution2D",
     "UniformGrid1D",
+    "UniformGrid2D",
     "compile_complex_pde_solution_scene",
+    "compile_scalar_pde_solution_2d_scene",
     "compile_scalar_pde_solution_scene",
     "complex_second_derivative_1d",
+    "laplacian_2d",
     "second_derivative_1d",
 ]
