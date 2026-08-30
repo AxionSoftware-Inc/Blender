1ef3f8101f22da58020595814c860cbf946e70fe
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 17:30:41 +0500

    Export curvature and geodesic APIs

--- spectra/domains/differential_geometry/__init__.py
+++ spectra/domains/differential_geometry/__init__.py
@@ -7,10 +7,21 @@ from spectra.domains.differential_geometry.domain import (
     metric_inner_product,
     metric_matrix,
     raise_index,
+    ricci_tensor,
+    riemann_curvature,
+    scalar_curvature,
+)
+from spectra.domains.differential_geometry.geodesics import (
+    GeodesicProblem,
+    GeodesicSolution,
+    GeodesicsDomain,
 )
 
 __all__ = [
     "DifferentialGeometryDomain",
+    "GeodesicProblem",
+    "GeodesicSolution",
+    "GeodesicsDomain",
     "MetricTensorField",
     "christoffel_symbols",
     "inverse_metric",
@@ -18,4 +29,7 @@ __all__ = [
     "metric_inner_product",
     "metric_matrix",
     "raise_index",
+    "ricci_tensor",
+    "riemann_curvature",
+    "scalar_curvature",
 ]
