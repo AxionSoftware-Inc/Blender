891318bd0447c3bd7f6c886a922b4e4aed9259d9
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:44:00 +0500

    Expose linear algebra domain

---
+++ spectra/domains/linear_algebra/__init__.py
@@ -0,0 +1,9 @@
+from spectra.domains.linear_algebra.domain import (
+    LinearAlgebraDomain,
+    VectorN,
+    inner_product,
+    norm,
+    normalize,
+)
+
+__all__ = ["LinearAlgebraDomain", "VectorN", "inner_product", "norm", "normalize"]
