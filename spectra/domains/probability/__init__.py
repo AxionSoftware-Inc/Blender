24a7afef1fe0874e25726b7eb232fa3e020e29e0
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 14:04:32 +0500

    Export continuous probability domain

--- spectra/domains/probability/__init__.py
+++ spectra/domains/probability/__init__.py
@@ -1,3 +1,8 @@
+from spectra.domains.probability.continuous import (
+    ContinuousDistribution1D,
+    ContinuousProbabilityDomain,
+    compile_continuous_distribution_scene,
+)
 from spectra.domains.probability.domain import (
     DiscreteDistribution,
     Outcome,
@@ -7,9 +12,12 @@ from spectra.domains.probability.domain import (
 )
 
 __all__ = [
+    "ContinuousDistribution1D",
+    "ContinuousProbabilityDomain",
     "DiscreteDistribution",
     "Outcome",
     "ProbabilityDomain",
+    "compile_continuous_distribution_scene",
     "expectation",
     "variance",
 ]
