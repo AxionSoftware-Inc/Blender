38fe06676a5a50bb30c5487471fc4afd13bc5c10
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 16:45:25 +0500

    math: export complex PDE API

--- spectra/domains/partial_differential_equations/__init__.py
+++ spectra/domains/partial_differential_equations/__init__.py
@@ -1,3 +1,10 @@
+from spectra.domains.partial_differential_equations.complex_domain import (
+    ComplexPDEProblem1D,
+    ComplexPDESolution1D,
+    ComplexPartialDifferentialEquationsDomain,
+    compile_complex_pde_solution_scene,
+    complex_second_derivative_1d,
+)
 from spectra.domains.partial_differential_equations.domain import (
     BoundaryMode1D,
     PartialDifferentialEquationsDomain,
@@ -12,10 +19,15 @@ from spectra.domains.partial_differential_equations.visualization import (
 
 __all__ = [
     "BoundaryMode1D",
+    "ComplexPDEProblem1D",
+    "ComplexPDESolution1D",
+    "ComplexPartialDifferentialEquationsDomain",
     "PartialDifferentialEquationsDomain",
     "ScalarPDEProblem1D",
     "ScalarPDESolution1D",
     "UniformGrid1D",
+    "compile_complex_pde_solution_scene",
     "compile_scalar_pde_solution_scene",
+    "complex_second_derivative_1d",
     "second_derivative_1d",
 ]
