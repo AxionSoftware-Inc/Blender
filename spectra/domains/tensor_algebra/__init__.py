d7ebd8ce75a1600ad0acf3b0a962adb536c7c001
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 16:52:43 +0500

    math: export tensor algebra domain

---
+++ spectra/domains/tensor_algebra/__init__.py
@@ -0,0 +1,21 @@
+from spectra.domains.tensor_algebra.domain import (
+    Tensor,
+    TensorAlgebraDomain,
+    contract,
+    outer_product,
+    permute_axes,
+    tensor_add,
+    tensor_scale,
+    trace,
+)
+
+__all__ = [
+    "Tensor",
+    "TensorAlgebraDomain",
+    "contract",
+    "outer_product",
+    "permute_axes",
+    "tensor_add",
+    "tensor_scale",
+    "trace",
+]
