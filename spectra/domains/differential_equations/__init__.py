54ef0c54a4ca0d14f5ba5c485c5a4f2e2306e216
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 23:20:04 +0500

    Export RK4 provenance API

--- spectra/domains/differential_equations/__init__.py
+++ spectra/domains/differential_equations/__init__.py
@@ -2,12 +2,16 @@ from spectra.domains.differential_equations.domain import (
     DifferentialEquationsDomain,
     FirstOrderSystem,
     ODESolution,
+    RK4_METHOD,
     solve_rk4,
+    solve_rk4_tracked,
 )
 
 __all__ = [
     "DifferentialEquationsDomain",
     "FirstOrderSystem",
     "ODESolution",
+    "RK4_METHOD",
     "solve_rk4",
+    "solve_rk4_tracked",
 ]
