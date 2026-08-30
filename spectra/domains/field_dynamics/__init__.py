4f078795a09da3fba158b781d238fbdb9f7ba636
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 18:10:15 +0500

    Export field dynamics domain

---
+++ spectra/domains/field_dynamics/__init__.py
@@ -0,0 +1,15 @@
+from spectra.domains.field_dynamics.domain import (
+    CurveSolution3D,
+    FieldDynamicsDomain,
+    IntegralCurveProblem3D,
+    PathlineProblem3D,
+)
+from spectra.domains.field_dynamics.visualization import compile_curve_solution_scene
+
+__all__ = [
+    "CurveSolution3D",
+    "FieldDynamicsDomain",
+    "IntegralCurveProblem3D",
+    "PathlineProblem3D",
+    "compile_curve_solution_scene",
+]
