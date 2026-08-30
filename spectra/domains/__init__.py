703516afcc774d13bc1ae2ea02ff567e304cec2a
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 15:59:41 +0500

    Expose domain catalog APIs

--- spectra/domains/__init__.py
+++ spectra/domains/__init__.py
@@ -1,4 +1,14 @@
 from spectra.domains.base import DomainModule
+from spectra.domains.builtin_catalog import builtin_domain_catalog
+from spectra.domains.catalog import DomainCatalog, DomainDescriptor
 from spectra.domains.registry import DomainDependency, DomainRegistry, DomainResolutionError
 
-__all__ = ["DomainDependency", "DomainModule", "DomainRegistry", "DomainResolutionError"]
+__all__ = [
+    "DomainCatalog",
+    "DomainDependency",
+    "DomainDescriptor",
+    "DomainModule",
+    "DomainRegistry",
+    "DomainResolutionError",
+    "builtin_domain_catalog",
+]
