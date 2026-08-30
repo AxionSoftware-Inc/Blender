f19a9260d16e04e21e2c5983a97136d1a7ecad3f
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 18:42:19 +0500

    Export tensor field domain

---
+++ spectra/domains/tensor_fields/__init__.py
@@ -0,0 +1,11 @@
+from spectra.domains.tensor_fields.domain import (
+    TensorField3D,
+    TensorFieldsDomain,
+    TimeDependentTensorField3D,
+)
+
+__all__ = [
+    "TensorField3D",
+    "TensorFieldsDomain",
+    "TimeDependentTensorField3D",
+]
