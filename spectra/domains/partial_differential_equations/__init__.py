1b673a68bc070dbb1fb40404b5d589e5be36456d
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 18:48:31 +0500

    Export expanded 2D PDE numerical foundation

--- spectra/domains/partial_differential_equations/__init__.py
+++ spectra/domains/partial_differential_equations/__init__.py
@@ -5,6 +5,11 @@ from spectra.domains.partial_differential_equations.complex_domain import (
     compile_complex_pde_solution_scene,
     complex_second_derivative_1d,
 )
+from spectra.domains.partial_differential_equations.complex2d import (
+    ComplexPDE2DDomain,
+    ComplexPDEProblem2D,
+    ComplexPDESolution2D,
+)
 from spectra.domains.partial_differential_equations.domain import (
     BoundaryMode1D,
     PartialDifferentialEquationsDomain,
@@ -29,13 +34,33 @@ from spectra.domains.partial_differential_equations.elliptic2d import (
 from spectra.domains.partial_differential_equations.field_adapters2d import (
     PDEFieldAdapters2DDomain,
     scalar_field_from_grid_2d,
+    time_scalar_field_from_grid_2d,
+    time_vector_field_from_grid_2d,
     vector_field_from_grid_2d,
+)
+from spectra.domains.partial_differential_equations.integrals2d import (
+    GridIntegrals2DDomain,
+    integrate_scalar_grid_2d,
+    integrate_vector_magnitude_squared_grid_2d,
+    scalar_l2_norm_grid_2d,
 )
 from spectra.domains.partial_differential_equations.operators2d import (
     PDEOperators2DDomain,
+    curl_grid_2d,
     divergence_grid_2d,
     gradient_grid_2d,
     vector_upwind_advection_grid_2d,
+)
+from spectra.domains.partial_differential_equations.second_order2d import (
+    SecondOrderPDE2DDomain,
+    SecondOrderScalarPDEProblem2D,
+    SecondOrderScalarPDESolution2D,
+)
+from spectra.domains.partial_differential_equations.stability2d import (
+    ExplicitStability2D,
+    Stability2DDomain,
+    explicit_stability_for_field_2d,
+    explicit_stability_from_samples_2d,
 )
 from spectra.domains.partial_differential_equations.transport2d import (
     AdvectionDiffusionProblem2D,
@@ -55,10 +80,15 @@ __all__ = [
     "AdvectionDiffusionSolution2D",
     "BoundaryMode1D",
     "BoundaryMode2D",
+    "ComplexPDE2DDomain",
     "ComplexPDEProblem1D",
+    "ComplexPDEProblem2D",
     "ComplexPDESolution1D",
+    "ComplexPDESolution2D",
     "ComplexPartialDifferentialEquationsDomain",
     "EllipticPDE2DDomain",
+    "ExplicitStability2D",
+    "GridIntegrals2DDomain",
     "PDEFieldAdapters2DDomain",
     "PDEOperators2DDomain",
     "PartialDifferentialEquations2DDomain",
@@ -69,6 +99,10 @@ __all__ = [
     "ScalarPDEProblem2D",
     "ScalarPDESolution1D",
     "ScalarPDESolution2D",
+    "SecondOrderPDE2DDomain",
+    "SecondOrderScalarPDEProblem2D",
+    "SecondOrderScalarPDESolution2D",
+    "Stability2DDomain",
     "Transport2DDomain",
     "UniformGrid1D",
     "UniformGrid2D",
@@ -76,11 +110,19 @@ __all__ = [
     "compile_scalar_pde_solution_2d_scene",
     "compile_scalar_pde_solution_scene",
     "complex_second_derivative_1d",
+    "curl_grid_2d",
     "divergence_grid_2d",
+    "explicit_stability_for_field_2d",
+    "explicit_stability_from_samples_2d",
     "gradient_grid_2d",
+    "integrate_scalar_grid_2d",
+    "integrate_vector_magnitude_squared_grid_2d",
     "laplacian_2d",
     "scalar_field_from_grid_2d",
+    "scalar_l2_norm_grid_2d",
     "second_derivative_1d",
+    "time_scalar_field_from_grid_2d",
+    "time_vector_field_from_grid_2d",
     "upwind_advection_2d",
     "vector_field_from_grid_2d",
     "vector_upwind_advection_grid_2d",
