1964a13944aaf62474d9ca0d83002a24afa11617
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 15:48:35 +0500

    Export time-dependent mathematical fields

--- spectra/domains/mathematics/__init__.py
+++ spectra/domains/mathematics/__init__.py
@@ -1,9 +1,17 @@
 from spectra.domains.mathematics.domain import MathematicsDomain
-from spectra.domains.mathematics.fields import RegularGrid3D, ScalarField3D, VectorField3D
+from spectra.domains.mathematics.fields import (
+    AxisSample,
+    RegularGrid3D,
+    ScalarField3D,
+    TimeDependentScalarField3D,
+    TimeDependentVectorField3D,
+    VectorField3D,
+)
 from spectra.domains.mathematics.functions import Function1D, Function2D, Interval, RectDomain2D
 from spectra.domains.mathematics.parametric import ParametricCurve3D, ParametricSurface3D
 
 __all__ = [
+    "AxisSample",
     "Function1D",
     "Function2D",
     "Interval",
@@ -13,5 +21,7 @@ __all__ = [
     "RectDomain2D",
     "RegularGrid3D",
     "ScalarField3D",
+    "TimeDependentScalarField3D",
+    "TimeDependentVectorField3D",
     "VectorField3D",
 ]
