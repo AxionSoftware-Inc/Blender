bf164c85a931cfe6df230545606855b0cb9c426e
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:51:40 +0500

    Export differential-equation domain

---
+++ spectra/domains/differential_equations/__init__.py
@@ -0,0 +1,13 @@
+from spectra.domains.differential_equations.domain import (
+    DifferentialEquationsDomain,
+    FirstOrderSystem,
+    ODESolution,
+    solve_rk4,
+)
+
+__all__ = [
+    "DifferentialEquationsDomain",
+    "FirstOrderSystem",
+    "ODESolution",
+    "solve_rk4",
+]
