e1a2b913d17e10cc1f57fac5cd7b956b1fbf11d2
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 18:25:02 +0500

    Export transport, elliptic, and grid operator PDE domains

--- spectra/domains/partial_differential_equations/__init__.py
+++ spectra/domains/partial_differential_equations/__init__.py
@@ -21,6 +21,23 @@ from spectra.domains.partial_differential_equations.domain2d import (
     UniformGrid2D,
     laplacian_2d,
 )
+from spectra.domains.partial_differential_equations.elliptic2d import (
+    EllipticPDE2DDomain,
+    PoissonProblem2D,
+    PoissonSolution2D,
+)
+from spectra.domains.partial_differential_equations.operators2d import (
+    PDEOperators2DDomain,
+    divergence_grid_2d,
+    gradient_grid_2d,
+    vector_upwind_advection_grid_2d,
+)
+from spectra.domains.partial_differential_equations.transport2d import (
+    AdvectionDiffusionProblem2D,
+    AdvectionDiffusionSolution2D,
+    Transport2DDomain,
+    upwind_advection_2d,
+)
 from spectra.domains.partial_differential_equations.visualization import (
     compile_scalar_pde_solution_scene,
 )
@@ -29,23 +46,34 @@ from spectra.domains.partial_differential_equations.visualization2d import (
 )
 
 __all__ = [
+    "AdvectionDiffusionProblem2D",
+    "AdvectionDiffusionSolution2D",
     "BoundaryMode1D",
     "BoundaryMode2D",
     "ComplexPDEProblem1D",
     "ComplexPDESolution1D",
     "ComplexPartialDifferentialEquationsDomain",
+    "EllipticPDE2DDomain",
+    "PDEOperators2DDomain",
     "PartialDifferentialEquations2DDomain",
     "PartialDifferentialEquationsDomain",
+    "PoissonProblem2D",
+    "PoissonSolution2D",
     "ScalarPDEProblem1D",
     "ScalarPDEProblem2D",
     "ScalarPDESolution1D",
     "ScalarPDESolution2D",
+    "Transport2DDomain",
     "UniformGrid1D",
     "UniformGrid2D",
     "compile_complex_pde_solution_scene",
     "compile_scalar_pde_solution_2d_scene",
     "compile_scalar_pde_solution_scene",
     "complex_second_derivative_1d",
+    "divergence_grid_2d",
+    "gradient_grid_2d",
     "laplacian_2d",
     "second_derivative_1d",
+    "upwind_advection_2d",
+    "vector_upwind_advection_grid_2d",
 ]
