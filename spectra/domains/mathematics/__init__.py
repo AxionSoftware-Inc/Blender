80435e90c4778056d9b9e62fd39856f177ca5192
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:41:51 +0500

    Expose foundational mathematics domain

---
+++ spectra/domains/mathematics/__init__.py
@@ -0,0 +1,4 @@
+from spectra.domains.mathematics.domain import MathematicsDomain
+from spectra.domains.mathematics.functions import Function1D, Interval
+
+__all__ = ["MathematicsDomain", "Function1D", "Interval"]
