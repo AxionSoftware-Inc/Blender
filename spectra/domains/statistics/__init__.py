c8193a28d68a359af250f2dd335655f92bb77380
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:57:01 +0500

    Export statistics domain

---
+++ spectra/domains/statistics/__init__.py
@@ -0,0 +1,23 @@
+from spectra.domains.statistics.domain import (
+    Dataset1D,
+    Histogram,
+    HistogramBin,
+    StatisticsDomain,
+    SummaryStatistics,
+    histogram,
+    mean,
+    sample_variance,
+    summarize,
+)
+
+__all__ = [
+    "Dataset1D",
+    "Histogram",
+    "HistogramBin",
+    "StatisticsDomain",
+    "SummaryStatistics",
+    "histogram",
+    "mean",
+    "sample_variance",
+    "summarize",
+]
