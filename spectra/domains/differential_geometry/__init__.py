cb5c1cf7959dbeb7855ec875a9068fb87defd82a
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 16:57:49 +0500

    math: export differential geometry domain

---
+++ spectra/domains/differential_geometry/__init__.py
@@ -0,0 +1,21 @@
+from spectra.domains.differential_geometry.domain import (
+    DifferentialGeometryDomain,
+    MetricTensorField,
+    christoffel_symbols,
+    inverse_metric,
+    lower_index,
+    metric_inner_product,
+    metric_matrix,
+    raise_index,
+)
+
+__all__ = [
+    "DifferentialGeometryDomain",
+    "MetricTensorField",
+    "christoffel_symbols",
+    "inverse_metric",
+    "lower_index",
+    "metric_inner_product",
+    "metric_matrix",
+    "raise_index",
+]
