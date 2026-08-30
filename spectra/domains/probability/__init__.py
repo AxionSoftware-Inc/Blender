67f023dc1e53b88c232e7f90553f09f7c0439585
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 10:43:38 +0500

    Expose probability domain

---
+++ spectra/domains/probability/__init__.py
@@ -0,0 +1,15 @@
+from spectra.domains.probability.domain import (
+    DiscreteDistribution,
+    Outcome,
+    ProbabilityDomain,
+    expectation,
+    variance,
+)
+
+__all__ = [
+    "DiscreteDistribution",
+    "Outcome",
+    "ProbabilityDomain",
+    "expectation",
+    "variance",
+]
