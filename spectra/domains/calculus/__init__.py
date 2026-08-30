726a0609ac361d796d3f1d49091eeba7a044d7a8
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 15:56:16 +0500

    Export vector calculus capabilities

--- spectra/domains/calculus/__init__.py
+++ spectra/domains/calculus/__init__.py
@@ -1,9 +1,21 @@
 from spectra.domains.calculus.domain import (
     CalculusDomain,
     TangentSample,
+    curl_at,
     derivative_at,
+    divergence_at,
+    gradient_at,
     integrate,
     tangent_at,
 )
 
-__all__ = ["CalculusDomain", "TangentSample", "derivative_at", "integrate", "tangent_at"]
+__all__ = [
+    "CalculusDomain",
+    "TangentSample",
+    "curl_at",
+    "derivative_at",
+    "divergence_at",
+    "gradient_at",
+    "integrate",
+    "tangent_at",
+]
