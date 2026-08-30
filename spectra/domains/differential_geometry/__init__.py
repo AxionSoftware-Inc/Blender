907c091ce08ace3debc0d1da1bccc758b087182b
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-30 17:35:00 +0500

    Export geodesic view API

--- spectra/domains/differential_geometry/__init__.py
+++ spectra/domains/differential_geometry/__init__.py
@@ -14,16 +14,20 @@ from spectra.domains.differential_geometry.domain import (
 from spectra.domains.differential_geometry.geodesics import (
     GeodesicProblem,
     GeodesicSolution,
+    GeodesicView3D,
     GeodesicsDomain,
 )
+from spectra.domains.differential_geometry.visualization import compile_geodesic_view_scene
 
 __all__ = [
     "DifferentialGeometryDomain",
     "GeodesicProblem",
     "GeodesicSolution",
+    "GeodesicView3D",
     "GeodesicsDomain",
     "MetricTensorField",
     "christoffel_symbols",
+    "compile_geodesic_view_scene",
     "inverse_metric",
     "lower_index",
     "metric_inner_product",
