708e5a585cdbe12a37b6e593a81acd9d9c31b067
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-01 08:22:39 +0500

    Export adaptive RK45 solver APIs

--- spectra/domains/differential_equations/__init__.py
+++ spectra/domains/differential_equations/__init__.py
@@ -1,3 +1,9 @@
+from spectra.domains.differential_equations.adaptive import (
+    AdaptiveReferenceSolversDomain,
+    RK45_EXECUTION,
+    RK45_METHOD,
+    solve_rk45,
+)
 from spectra.domains.differential_equations.domain import (
     DifferentialEquationsDomain,
     FirstOrderSystem,
@@ -16,16 +22,20 @@ from spectra.domains.differential_equations.reference_solvers import (
 )
 
 __all__ = [
+    "AdaptiveReferenceSolversDomain",
     "DifferentialEquationsDomain",
     "FirstOrderSystem",
     "HEUN_EXECUTION",
     "HEUN_METHOD",
     "ODE_FIRST_ORDER_SOLVER_ROLE",
     "ODESolution",
+    "RK45_EXECUTION",
+    "RK45_METHOD",
     "RK4_EXECUTION",
     "RK4_METHOD",
     "ReferenceODESolversDomain",
     "solve_heun",
+    "solve_rk45",
     "solve_rk4",
     "solve_rk4_tracked",
 ]
