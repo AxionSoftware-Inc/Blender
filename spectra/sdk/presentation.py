3b76f6857db1b299c99c4a07d7fb65a6206821ea
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-04 03:41:26 +0500

    Expose quantitative presentation color helpers in SDK

--- spectra/sdk/presentation.py
+++ spectra/sdk/presentation.py
@@ -1,3 +1,20 @@
+from spectra.color_scales import (
+    colorize_primitive,
+    colorize_scene,
+    map_scalar_values,
+    resolve_scalar_range,
+    sample_palette,
+)
 from spectra.presentation import compose_presentation, merge_timelines, staggered_reveal
 from spectra.presentation_models import *
 
+__all__ = [
+    "compose_presentation",
+    "merge_timelines",
+    "staggered_reveal",
+    "colorize_primitive",
+    "colorize_scene",
+    "map_scalar_values",
+    "resolve_scalar_range",
+    "sample_palette",
+]
