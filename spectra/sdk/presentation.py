279a50dc269681f19b64d1f847f27f553a6542e4
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-04 03:41:34 +0500

    Preserve presentation SDK star export compatibility

--- spectra/sdk/presentation.py
+++ spectra/sdk/presentation.py
@@ -7,14 +7,3 @@ from spectra.color_scales import (
 )
 from spectra.presentation import compose_presentation, merge_timelines, staggered_reveal
 from spectra.presentation_models import *
-
-__all__ = [
-    "compose_presentation",
-    "merge_timelines",
-    "staggered_reveal",
-    "colorize_primitive",
-    "colorize_scene",
-    "map_scalar_values",
-    "resolve_scalar_range",
-    "sample_palette",
-]
