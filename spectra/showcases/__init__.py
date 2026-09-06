0581786f1d7e05953f682ed0acc506e3c7a38d1d
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-06 18:14:25 +0500

    Export quantum wavepacket showcase

--- spectra/showcases/__init__.py
+++ spectra/showcases/__init__.py
@@ -15,12 +15,26 @@ from .maxwell_wave import (
     build_maxwell_wave_base_scene,
     build_maxwell_wave_scene,
 )
+from .quantum_wavepacket import (
+    PROBABILITY_DENSITY_2D,
+    RADIAN,
+    QuantumWavepacketShowcaseConfig,
+    build_quantum_wavepacket_base_scene,
+    build_quantum_wavepacket_scene,
+    solve_quantum_wavepacket_showcase,
+)
 
 __all__ = [
     "ElectrostaticLabShowcaseConfig",
     "MaxwellWaveShowcaseConfig",
+    "PROBABILITY_DENSITY_2D",
+    "QuantumWavepacketShowcaseConfig",
+    "RADIAN",
     "build_electrostatic_lab_base_scene",
     "build_electrostatic_lab_scene",
     "build_maxwell_wave_base_scene",
     "build_maxwell_wave_scene",
+    "build_quantum_wavepacket_base_scene",
+    "build_quantum_wavepacket_scene",
+    "solve_quantum_wavepacket_showcase",
 ]
